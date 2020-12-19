a54893624bbffdbbb23088cf8720673185cb6cfb
grjus <grjus_services+github@pm.me>
2020-12-19 22:43:54 +0100

    Added fatigue stress calculation

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 export { calculateStressRatio } from "./src/Fatigue.js";
 export { calculateAlternatingStress } from "./src/Fatigue.js";
 export { calculateMeanStress } from "./src/Fatigue.js";
+export { calculateFatigueStress } from "./src/Fatigue.js";
 export { getDeratingFactor } from "./src/ModificationFactors.js";
 export { calculateDamage } from "./src/Damage.js";
