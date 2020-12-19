cfc9d1bb530ec1b372dd3fd34e8b926ed0933e4e
grjus <grjus_services+github@pm.me>
2020-12-19 18:32:25 +0100

    Updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
 # structural-fatigue
-Structural fatigue calculator
+
+structural-fatigue is javascript library for basic structural fatigue calculation
+
+## Installation
+
+Use the package manager [Node.js](http://nodejs.org/) to install structural-fatigue.
+
+```bash
+npm install structural-fatigue
+```
+
+## Usage
+
+**This library is valid for ksi stress unit only**
+
+```javascript
+import { calculateFatigueStress } from "./Fatigue.js";
+import { getDeratingFactor } from "./ModificationFactors.js";
+import { calculateDamage } from "./Damage.js";
+
+const modificationFactor = getDeratingFactor({
+  relLevel: 66,
+  loadType: "axial",
+  surfFinish: ["RHR125", 155],
+  miscFactor: 0.56,
+});
+
+const minStress = [-10, -20, -30, -50];
+const maxStress = [20, 30, 50, 30];
+
+const fatigueStress = minStress.map((item, idx) =>
+  calculateFatigueStress(
+    item,
+    maxStress[idx],
+    "GOODMAN",
+    { ultStrength: 155 },
+    true
+  )
+);
+
+const requiredCycles = [200_000, 400_000, 300_000, 200_000];
+
+const damage = requiredCycles.map((item, idx) =>
+  calculateDamage(fatigueStress[idx], item, -155, modificationFactor)
+);
+```
--- src/index.js
+++ src/index.js
@@ -1,54 +1,31 @@
-// import { calculateFatigueStress } from "./Fatigue.js";
-// import { getDeratingFactor } from "./ModificationFactors.js";
-// import { calculateDamage } from "./Damage.js";
+import { calculateFatigueStress } from "./Fatigue.js";
+import { getDeratingFactor } from "./ModificationFactors.js";
+import { calculateDamage } from "./Damage.js";
 
-// const modificationFactor = getDeratingFactor({
-//   relLevel: 66,
-//   loadType: "axial",
-//   surfFinish: ["RHR125", 155],
-//   miscFactor: 0.56,
-// });
-
-// const minStress = [-10, -20, -30, -50];
-// const maxStress = [20, 30, 50, 30];
-
-// const fatigueStress = minStress.map((item, idx) =>
-//   calculateFatigueStress(
-//     item,
-//     maxStress[idx],
-//     "GOODMAN",
-//     { ultStrength: 155 },
-//     true
-//   )
-// );
-
-// const requiredCycles = [200_000, 400_000, 300_000, 200_000];
-
-// const damage = requiredCycles.map((item, idx) =>
-//   calculateDamage(fatigueStress[idx], item, -155, modificationFactor)
-// );
-
-// console.log(damage);
-
-function NameException(message) {
-  this.message = "You fucked up";
-  this.name = NameException;
-}
-
-function Dummy(name) {
-  this._name = name;
-}
-
-Object.defineProperty(Dummy.prototype, "name", {
-  get: function () {
-    return this._name;
-  },
-  set: function (value) {
-    if (value.length < 4) {
-      throw new NameException();
-    }
-    this._name = value;
-  },
+const modificationFactor = getDeratingFactor({
+  relLevel: 66,
+  loadType: "axial",
+  surfFinish: ["RHR125", 155],
+  miscFactor: 0.56,
 });
-const lol = new Dummy("as");
-console.log(lol.name);
+
+const minStress = [-10, -20, -30, -50];
+const maxStress = [20, 30, 50, 30];
+
+const fatigueStress = minStress.map((item, idx) =>
+  calculateFatigueStress(
+    item,
+    maxStress[idx],
+    "GOODMAN",
+    { ultStrength: 155 },
+    true
+  )
+);
+
+const requiredCycles = [200_000, 400_000, 300_000, 200_000];
+
+const damage = requiredCycles.map((item, idx) =>
+  calculateDamage(fatigueStress[idx], item, -155, modificationFactor)
+);
+
+console.log(damage);
